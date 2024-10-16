82d7040fa24d8c4ede70b6b625314347bbfd7fa8
Mu12Lambda11 <89223075+Mu12Lambda11@users.noreply.github.com>
2024-10-16 12:59:50 -0700

    Initial draft of Spotify API

--- SpotifyConnect.py
+++ SpotifyConnect.py
@@ -1,6 +1,26 @@
 import spotipy
 import creds
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
 
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=creds.spotipy_client_id,
+                                               client_secret=creds.spotipy_client_secret,
+                                               redirect_uri='https://localhost:3000',
+                                               scope='playlist-modify-public'))
 
+#@param List:songs_array
+#Searches for the songs in spotify's database, and adds them to a list of uris if found
+#Afterwards, generates a playlist using these found songs.
+def search_songs(songs_array):
+    song_uris=[]
+    for song in songs_array:
+        query = f"track:{song['track']} artist:{song['artist']} year:{song['year']}"
+        result= sp.search(q=query, type='track', limit=1)
+        if result ['tracks']['items']:
+            song_uris.append(result ['tracks']['items']['uri'])
+    
+    user_id= sp.current_user()['id']
+    playlist = sp.user_playlist_create(user=user_id, name="Generated Playlist", public=True)    
+    sp.playlist_add_items(playlist_id=playlist['id'], items=song_uris)    
+    
+    
 
